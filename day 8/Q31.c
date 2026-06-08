01f9ef9345732af9dfee5a78da233bec16e80175
shashank896523 <121956757+shashank896523@users.noreply.github.com>
2026-06-08 22:31:11 +0530

    Add files via upload

---
+++ day 8/Q31.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+        int i,j;
+        for(i=1;i<=5;i++){
+            int x=65;
+        for(j=1;j<=i;j++){
+            printf("%c",x);
+            x++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
